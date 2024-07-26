592e3fb426a7f3e8e720373de535c7370f3cf0c7
risyanoviyanti <144987452+risyanoviyanti@users.noreply.github.com>
2024-07-26 11:50:31 +0700

    Add files via upload

---
+++ bab2soal2/src/bab2soal2/Bab2soal2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab2soal2;
+
+/**
+ *
+ * @author admin
+ */
+public class Bab2soal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String makanan = "ayam geprek";
+        String minuman = "Jus Jambu";
+        
+        System.out.println("makanan favorit saya : " + makanan);
+        System.out.println("minuman favorit saya : " + minuman);
+    }
+    
+}
