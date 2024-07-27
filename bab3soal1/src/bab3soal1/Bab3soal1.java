f2c6b2fe5831b1557d03c818d1512b9a8083e955
risyanoviyanti <144987452+risyanoviyanti@users.noreply.github.com>
2024-07-27 20:56:53 +0700

    Add files via upload

---
+++ bab3soal1/src/bab3soal1/Bab3soal1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab3soal1;
+
+/**
+ *
+ * @author admin
+ */
+public class Bab3soal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   int Var1=9;
+   float Var2=8.99f;
+   char Var3='A';
+   boolean Var4=false;
+   String Var5="Hello World";
+   
+ System.out.println(Var1);
+   
+    }
+    
+    
+}
---
+++ bab3soal2/src/bab3soal2/Bab3soal2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab3soal2;
+
+/**
+ *
+ * @author admin
+ */
+public class Bab3soal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
