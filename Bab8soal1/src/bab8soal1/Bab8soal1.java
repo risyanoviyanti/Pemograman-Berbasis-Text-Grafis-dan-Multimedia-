e6f1110036d8b2d1a711a622e7045cddacc7bcba
risyanoviyanti <144987452+risyanoviyanti@users.noreply.github.com>
2024-08-29 09:23:15 +0700

    Add files via upload

---
+++ Bab8soal1/src/bab8soal1/Bab8soal1.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab8soal1;
+
+/**
+ *
+ * @author admin
+ */
+import java.util.Scanner;
+public class Bab8soal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner myInput = new Scanner(System.in);
+        
+        System.out.println( "Nilai Awal ?: ");
+        int nilaiAwal = myInput.nextInt();
+        
+        System.out.println("Nilai Akhir ?: ");
+        int nilaiAkhir = myInput.nextInt();
+        
+        System.out.println("Hasilnya :");
+        int j = 1;
+        for (int i = nilaiAwal; i<= nilaiAkhir; i +=5) {
+            System.out.println(j + ". " + i);
+            j++;
+        }
+       }
+    
+}
