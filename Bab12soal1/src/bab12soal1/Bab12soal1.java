6b38dee47d1515629066190dfec488aab4c24e15
risyanoviyanti <144987452+risyanoviyanti@users.noreply.github.com>
2024-09-11 07:15:45 +0700

    Add files via upload

---
+++ Bab12soal1/src/bab12soal1/Bab12soal1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab12soal1;
+
+/**
+ *
+ * @author admin
+ */
+public class Bab12soal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         class Karyawan {
+    String nama;
+    double gaji;
+
+    public Karyawan(String nama, double gaji) {
+        this.nama = nama;
+        this.gaji = gaji;
+    }
+}
+
+    }
+    
+}
